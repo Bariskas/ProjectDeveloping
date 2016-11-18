8845cd01c54b7d07da32b3f0a6277fb09c283506
Bariskas <Bariskas@gmail.com>
2016-11-18 04:19:09 +0300

    Создан проект для анимаций
    Создан проект для анимаций

---
+++ SfmlProjects/AnimationRectangles/stdafx.h
@@ -0,0 +1,22 @@
+// stdafx.h: включаемый файл дл€ стандартных системных включаемых файлов
+// или включаемых файлов дл€ конкретного проекта, которые часто используютс€, но
+// не часто измен€ютс€
+//
+
+#pragma once
+
+#include "targetver.h"
+
+#include <stdio.h>
+#include <tchar.h>
+
+#define _CRT_SECURE_NO_WARNINGS
+
+#define _USE_MATH_DEFINES
+#include <cmath>
+
+#include <SFML/Graphics.hpp>
+#include <SFML/Audio.hpp>
+#include <ctime>
+#include <iostream>
+#include <memory>
