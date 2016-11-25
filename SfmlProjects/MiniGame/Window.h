4196387385886aaefa2d9023a321c7092bf51c1a
Bariskas <Bariskas@gmail.com>
2016-11-25 04:03:57 +0300

    Добавлен проект для мини игры
    Добавлен проект для мини игры

---
+++ SfmlProjects/MiniGame/Window.h
@@ -0,0 +1,31 @@
+﻿#pragma once
+
+class Window
+{
+public:
+	Window();
+	Window(const std::string title, const sf::Vector2u& size);
+	~Window();
+
+	void BeginDraw();
+	void EndDraw();
+	void Update();
+
+	bool IsDone() const;
+	bool IsFullscreen() const;
+	sf::Vector2u GetWindowSize() const;
+
+	void ToggleFullscreen();
+
+	void Draw(sf::Drawable& drawable);
+private:
+	void Setup(const std::string& title, const sf::Vector2u size);
+	void Destroy();
+	void Create();
+
+	sf::RenderWindow m_window;
+	sf::Vector2u m_windowSize;
+	std::string m_windowTitle;
+	bool m_isDone;
+	bool m_isFullscreen;
+};
