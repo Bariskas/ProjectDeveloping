926fa44a331c900bba9a4aae293ec970491c8029
Boris Ibulaev <boris.ibulaev@ispringsolutions.com>
2016-11-25 19:55:57 +0300

    Added World class

--- SfmlProjects/Snake/Snake.cpp
+++ SfmlProjects/Snake/Snake.cpp
@@ -67,43 +67,54 @@ void Snake::Extend()
 	SnakeSegment& tail_head = m_snakeBody[m_snakeBody.size() - 1];
 	if (m_snakeBody.size() > 1)
 	{
-		SnakeSegment & tail_bone = m_snakeBody[m_snakeBody.size() - 1];
+		SnakeSegment & tail_bone = m_snakeBody[m_snakeBody.size() - 2];
 	
-		if (tail_head.position.x == tail_bone.position.x) {
-			if (tail_head.position.y > tail_bone.position.y) {
+		if (tail_head.position.x == tail_bone.position.x) 
+		{
+			if (tail_head.position.y > tail_bone.position.y) 
+			{
 				m_snakeBody.push_back(SnakeSegment(
 					tail_head.position.x, tail_head.position.y + 1));
 			}
-			else {
+			else 
+			{
 				m_snakeBody.push_back(SnakeSegment(
 					tail_head.position.x, tail_head.position.y - 1));
 			}
 		}
-		else if (tail_head.position.y == tail_bone.position.y) {
-			if (tail_head.position.x > tail_bone.position.x) {
+		else if (tail_head.position.y == tail_bone.position.y) 
+		{
+			if (tail_head.position.x > tail_bone.position.x) 
+			{
 				m_snakeBody.push_back(SnakeSegment(
 					tail_head.position.x + 1, tail_head.position.y));
 			}
-			else {
+			else 
+			{
 				m_snakeBody.push_back(SnakeSegment(
 					tail_head.position.x - 1, tail_head.position.y));
 			}
 		}
 	}
-	 else {
-		 if (m_dir == Direction::Up) {
+	else 
+	{
+		 if (m_dir == Direction::Up) 
+		 {
 			 m_snakeBody.push_back(SnakeSegment(
 				 tail_head.position.x, tail_head.position.y + 1));
 		 }
-		 else if (m_dir == Direction::Down) {
+		 else if (m_dir == Direction::Down) 
+		 {
 			 m_snakeBody.push_back(SnakeSegment(
 				 tail_head.position.x, tail_head.position.y - 1));
 		 }
-		 else if (m_dir == Direction::Left) {
+		 else if (m_dir == Direction::Left) 
+		 {
 			 m_snakeBody.push_back(SnakeSegment(
 				 tail_head.position.x + 1, tail_head.position.y));
 		 }
-		 else if (m_dir == Direction::Right) {
+		 else if (m_dir == Direction::Right) 
+		 {
 			 m_snakeBody.push_back(SnakeSegment(
 				 tail_head.position.x - 1, tail_head.position.y));
 		 }
@@ -123,4 +134,93 @@ void Snake::Reset()
 	m_lives = 3;
 	m_score = 0;
 	m_isLost = false;
+}
+
+void Snake::Move()
+{
+	for (int i = m_snakeBody.size() - 1; i > 0; --i)
+	{
+		m_snakeBody[i].position = m_snakeBody[i - 1].position;
+	}
+	if (m_dir == Direction::Left)
+	{
+		--m_snakeBody[0].position.x;
+	}
+	else if (m_dir == Direction::Right)
+	{
+		++m_snakeBody[0].position.x;
+	}
+	else if (m_dir == Direction::Up)
+	{
+		--m_snakeBody[0].position.y;
+	}
+	else if (m_dir == Direction::Down)
+	{
+		++m_snakeBody[0].position.y;
+	}
+}
+
+void Snake::Tick()
+{
+	if (m_snakeBody.empty())
+	{
+		return;
+	}
+	if (m_dir == Direction::None)
+	{
+		return;
+	}
+	Move();
+	CheckCollisions();
+}
+
+void Snake::Cut(int segments)
+{
+	for (int i = 0; i < segments; ++i)
+	{
+		m_snakeBody.pop_back();
+	}
+	--m_lives;
+	if (!m_lives)
+	{
+		Lose();
+		return;
+	}
+}
+
+void Snake::Render(sf::RenderWindow & window)
+{
+	if (m_snakeBody.empty())
+	{
+		return;
+	}
+	auto head = m_snakeBody.begin();
+	m_bodyRect.setFillColor(sf::Color::Yellow);
+	m_bodyRect.setPosition(head->position.x * m_size, head->position.y * m_size);
+	window.draw(m_bodyRect);
+
+	m_bodyRect.setFillColor(sf::Color::Green);
+	for (auto it = m_snakeBody.begin() + 1; it != m_snakeBody.end(); ++it)
+	{
+		m_bodyRect.setPosition(it->position.x * m_size, it->position.y * m_size);
+		window.draw(m_bodyRect);
+	}
+}
+
+void Snake::CheckCollisions()
+{
+	if (m_snakeBody.size() < 5)
+	{
+		return;
+	}
+	SnakeSegment& head = m_snakeBody.front();
+	for (auto it = m_snakeBody.begin() + 1; it != m_snakeBody.end(); ++it)
+	{
+		if (it->position == head.position)
+		{
+			int segments = m_snakeBody.end() - it;
+			Cut(segments);
+			break;
+		}
+	}
 }
